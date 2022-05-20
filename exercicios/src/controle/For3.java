803df5fb54cee6d0ead4d78042556b2d603f5e51
Rachel de Araujo Rosa Ferreira <rachel.ferreira@nava.com.br>
2022-05-20 18:00:32 -0300

    80.Switch

---
+++ exercicios/src/controle/For3.java
@@ -0,0 +1,23 @@
+package controle;
+
+public class For3 {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]", i, j);
+			}
+			System.out.println();
+		}
+	}
+		
+/*		int i = 0;
+		for(; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Saiu do For...");
+		System.out.println(i);
+	}*/
+
+}
